b28ca05a269ba174b58f5fd0227dec0943b16353
ChaminduJaya <chaminduj96@gmail.com>
2024-04-28 11:45:36 +0530

    add total runs

--- pythonProject/Exercise/function exercise/cricket.py
+++ pythonProject/Exercise/function exercise/cricket.py
@@ -1,5 +1,6 @@
 def start_game():
-    pass
+    global total_runs 
+    total_runs=0
 def overs_in_match():
     
     over =0
@@ -13,12 +14,26 @@ def overs_in_match():
 def balls_in_over(over):
     ball = 0
     while ball<6 :
-        print(f'overs :{over}.{ball}')
+        print(f'total :{total_runs}  overs :{over}.{ball}',end='')
+        score_off_balls()
+        
         ball +=1
 
-   
+def score_off_balls ():
+    global total_runs
+    score = input('Score :')
+    if score.isdigit() :        #validation
+        score = int(score)
+        #if 0 <= score <5 or score ==6:
+        if score in (0,1,2,3,4,6):
+            runs = score 
+            total_runs += runs
+           
+    else :
+        print('not digit')
 
 def print_score():
     pass
 
+start_game()
 overs_in_match()
